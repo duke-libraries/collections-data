f3e4682bb9b4d270428aa1935e7c8e9e88573580
Cory Lown <cory.lown@duke.edu>
2015-04-28 14:03:37 -0400

    show ill and overlap data in analysis group show view

--- app/models/monograph_holding.rb
+++ app/models/monograph_holding.rb
@@ -1,3 +1,11 @@
 class MonographHolding < ActiveRecord::Base
   has_and_belongs_to_many :analysis_groups
+
+  def interlibrary_loan_count
+    InterlibraryLoan.where(oclc_number: self.oclc_number).count
+  end
+
+  def overlap_holdings_count
+    OverlapHolding.where(oclc_number: self.oclc_number).where(overlap_group_id: 1).first.shared_by
+  end
 end
